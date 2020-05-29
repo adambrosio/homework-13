4d7247504c53ec443022b15e48920a4a2c7c72cb
Andrew D'Ambrosio <adambrosio20@gmail.com>
2020-05-28 22:14:33 -0400

    modification

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger);
-INSERT INTO burgers (burger_name) VALUES ("Chicken Burger);
-INSERT INTO burgers (burger_name) VALUES ("Pizza Burger);
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Pizza Burger");
